ad971e83167079371475e149d12ba33e4f03f17d
weichi.chen <mithrilchen@gmail.com>
2018-03-04 22:47:46 +0800

    Support py3

--- setup.py
+++ setup.py
@@ -26,5 +26,7 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ]
 )
